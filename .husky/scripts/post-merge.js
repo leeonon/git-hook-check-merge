47179a9e768231fd9a5e335e31d448469d600476
LeeOnOn <v5sollee@gmail.com>
2022-11-20 17:36:51 +0800

    feat: update utils

--- .husky/scripts/post-merge.js
+++ .husky/scripts/post-merge.js
@@ -1,24 +1,28 @@
+// https://github.com/typicode/husky/issues/930
+
 const { execSync } = require('node:child_process');
-const { showConfirm } = require('./common');
+const { showConfirm, getCurrentBranch } = require('./utils');
 
 const CHECK_BRANCH = ['master'];
 
 function getMergeBranch() {
   // 从 reflog 提取合并进来的分支名
-  function getBranchNameFromReflog(reflogMessage) {
-    const reg = /@\{\d+\}: merge (.*):/;
-    return reg.exec(reflogMessage)[1];
-  }
-
+  const reg = /@\{\d+\}: merge (.*):/;
   const reflogMessage = execSync('git reflog -1', { encoding: 'utf8' });
-  const mergedBranchName = getBranchNameFromReflog(reflogMessage);
+  const mergedBranchName = reg.exec(reflogMessage)[1];
+
   return mergedBranchName;
 }
 
-(() => {
-  const branchName = getMergeBranch();
-  if (CHECK_BRANCH.includes(branchName)) {
-    showConfirm('当前分支', branchName);
+function main() {
+  const mergeBranch = getMergeBranch();
+
+  if (CHECK_BRANCH.includes(mergeBranch)) {
+    const currentBranch = getCurrentBranch();
+
+    showConfirm(currentBranch, mergeBranch);
     return;
   }
-})();
+}
+
+main();
---
+++ .husky/scripts/utils.js
@@ -0,0 +1,62 @@
+const readline = require('node:readline');
+const { execSync } = require('node:child_process');
+
+function getCurrentBranch() {
+  const branch = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf8' });
+  return branch.replace(/[\r\n]/g,"");
+}
+
+
+function showConfirm(currentBranch, mergeBranch) {
+  console.log(`\x1B[31m警告：你正在将${mergeBranch}分支合并到${currentBranch}分支, 这是一个危险操作 \x1B[0m`);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  process.on('SIGINT', function() {
+    console.log("got SIGINT");
+  });
+
+  process.on('uncaughtException', function(err) {
+      console.log('Caught exception: ' + err);
+  });
+
+  process.on('exit', function() {
+    console.log( "never see this log message" )
+  })
+
+  rl.on('SIGINT', () => {
+    console.log('🚀 ~ file: utils.js ~ line 33 ~ rl.on ~ SIGINT');
+  });
+
+  rl.on('SIGTSTP', () => {
+    // 这会重写 SIGTSTP，且防止程序进入后台。
+    console.log('捕获 SIGTSTP。');
+  });
+
+  rl.on('SIGCONT', () => {
+    // `prompt` 会自动恢复流
+    console.log('🚀 ~ file: utils.js ~ line 31 ~ rl.on ~ prompt', prompt);
+  });
+
+  rl.question(`确定要将 ${mergeBranch} 分支合并到当前分支吗？(y/n) `, (answer) => {
+    if (answer === 'y') {
+      rl.close();
+      process.exit(0);
+    } else {
+      console.log('\x1B[32m撤销合并中...\x1B[0m');
+      console.log('\x1B[36mgit reset --merge HEAD@{1}\x1B[0m');
+      execSync('git reset --merge HEAD@{1}');
+      console.log('\x1B[34m已撤销合并\x1B[0m');
+      rl.close();
+      process.exit(0);
+    }
+  });
+}
+
+module.exports = {
+  showConfirm,
+  getCurrentBranch
+}
